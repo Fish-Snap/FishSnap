5e396525c61dd24b750e4ff49b159eb2c8c1d98d
David <davidarrozaqi085@gmail.com>
2024-06-02 16:04:50 +0700

    feat: add customview password

---
+++ app/src/main/java/com/example/fishsnap/components/PasswordTextField.kt
@@ -0,0 +1,38 @@
+package com.example.fishsnap.components
+
+import android.content.Context
+import android.graphics.Color
+import android.text.Editable
+import android.text.InputType
+import android.text.TextWatcher
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatEditText
+import com.example.fishsnap.R
+import com.google.android.material.textfield.TextInputLayout
+
+class PasswordTextField @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null) :
+    AppCompatEditText(context, attrs) {
+
+    init {
+        inputType = InputType.TYPE_TEXT_VARIATION_PASSWORD or InputType.TYPE_CLASS_TEXT
+        addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                val parent = parent.parent
+                if (parent is TextInputLayout) {
+                    parent.errorIconDrawable = null
+                    if (!s.isNullOrEmpty() && s.length < 8) {
+                        parent.error = context.getString(R.string.error_password)
+                        parent.boxStrokeColor = context.getColor(R.color.sageGreen)
+                    } else {
+                        parent.error = null
+                        parent.boxStrokeColor = Color.TRANSPARENT
+                    }
+                }
+            }
+
+            override fun afterTextChanged(s: Editable?) {}
+        })
+    }
+}
